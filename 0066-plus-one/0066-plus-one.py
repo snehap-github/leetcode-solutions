d6176aa01071c174677f3beae37fd3ee5997f9f4
snehap-github <161669518+snehap-github@users.noreply.github.com>
2025-05-05 18:29:45 -0400

    Time: 0 ms (100%), Space: 17.7 MB (80.04%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,18 +1,12 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-
-        is_nine = False
         
         for i in range(len(digits))[::-1]:
             if digits[i] != 9:
                 digits[i] = digits[i] + 1
-                is_nine = False
                 return digits
-            else:
-                digits[i] = 0
-                is_nine = True
-                continue
+            digits[i] = 0
 
-        if is_nine:
+        if digits[i] == 0:
             digits.insert(0,1)
             return digits
